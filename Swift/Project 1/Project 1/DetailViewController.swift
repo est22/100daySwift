88f80353af38a3a084d919e57160833ea3ef8759
est22 <anlia.seoul@gmail.com>
2022-03-07 23:24:54 +0900

    Project 3: Social Media-UIBarButtonItem, UIActivityViewController

--- Swift/Project 1/Project 1/DetailViewController.swift
+++ Swift/Project 1/Project 1/DetailViewController.swift
@@ -21,6 +21,9 @@ class DetailViewController: UIViewController {
         
         
         navigationItem.largeTitleDisplayMode = .never
+        
+        // 1. share - add right Bar Button item
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -37,15 +40,20 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // 2. add function shareTapped()
+    @objc func shareTapped() {
+        // 3. get image
+        guard let image =  imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem // or else it will crash on iPad
+        present(vc, animated: true)
     }
-    */
+
+    
 
 }
