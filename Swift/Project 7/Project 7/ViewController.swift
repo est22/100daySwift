e5e50951246842c9e2a4694e74b141b29d1d4cf0
est22 <anlia.seoul@gmail.com>
2022-03-25 04:13:52 +0900

    Day 33: typo error

--- Swift/Project 7/Project 7/ViewController.swift
+++ Swift/Project 7/Project 7/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var petitions = [String]{}
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +21,7 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = "Title goes here"
         cell.detailTextLabel?.text = "Subtitle goes here"
         return cell
