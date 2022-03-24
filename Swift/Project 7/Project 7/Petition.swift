8e553bb08591f385d60fb59c07bd9e27c5458ea4
est22 <anlia.seoul@gmail.com>
2022-03-25 04:41:44 +0900

    Day 33: Parsing JSON using the Codable protocol

---
+++ Swift/Project 7/Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by Lia AN on 2022/03/25.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Swift/Project 7/Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Lia AN on 2022/03/25.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
